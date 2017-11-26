961a2df8df8e77e521c7f5a008462c1e264ec778
Japjot Singh <japjot@Japjots-MacBook-Pro.local>
2017-11-26 14:47:22 -0800

    initial commit

---
+++ APlusB.py
@@ -0,0 +1,20 @@
+# Uses python3
+# There are two ways of running this program:
+# 1. Run
+#     python3 APlusB.py
+# then enter two numbers and press ctrl-d/ctrl-z
+# 2. Save two numbers to a file -- say, dataset.txt.
+# Then run
+#     python3 APlusB.py < dataset.txt
+
+import sys
+
+input = sys.stdin.read()
+tokens = input.split()
+
+a = tokens[0]
+b = tokens[1]
+
+c = int(a) + int(b)
+print()
+print(c)
